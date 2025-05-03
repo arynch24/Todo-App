78798fb2b8997f99446e359c748ec3de0c3c2ad5
Aryan <arynch24@gmail.com>
2025-05-03 05:52:44 +0530

    Add Home page component

---
+++ Frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <div className="w-full h-[600px] flex flex-col justify-center items-center">
+            <h1 className="text-6xl mb-4 font-bold text-zinc-800">The all-in-one work platform</h1>
+            <p className="text-2xl mb-16 text-zinc-700 text-center">Routine is the next-generation platform for professionals<br /> and teams to get things done faster!</p>
+            <button className="text-md bg-coral px-4 py-3 rounded-md text-white">Get Started</button>
+        </div>
+    )
+}
+
+export default Home

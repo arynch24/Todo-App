c7486971874a7d1763cc517399f5b058a958d6ee
Aryan <arynch24@gmail.com>
2025-05-05 10:03:55 +0530

    Add responsive menu bar and update home page layout

--- Frontend/src/components/NavBar.tsx
+++ Frontend/src/components/NavBar.tsx
@@ -1,29 +1,82 @@
 import { useNavigate } from "react-router-dom"
 import Cookies from "js-cookie"
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useState } from "react"
+
+const MenuBar = () => {
+  const navigate = useNavigate();
+  const token = Cookies.get('token');
+
+  return (
+    <div className="">
+      {
+        token ?
+          <div className="w-screen flex flex-col justify-between items-end gap-1 bg-[#ffffff] p-4 transition-normal">
+            <button className="w-full text-coral bg-[#FBEFEE] text-md font-semibold p-2 hover:bg-[#fddddd] transition-colors rounded-sm" onClick={() => navigate('/dashboard')}>
+              Dashboard</button>
+            <button className="w-full text-coral bg-[#FBEFEE] text-md font-semibold p-2 hover:bg-[#fddddd] transition-colors rounded-sm" onClick={() => {
+              Cookies.remove('token')
+              navigate('/')
+            }}>Logout</button>
+          </div>
+          :
+          <div className="w-screen flex flex-col justify-between items-end gap-1 bg-[#ffffff] p-4 transition-normal">
+            <button className="w-full text-coral bg-[#FBEFEE] text-md font-semibold p-2 hover:bg-[#fddddd] transition-colors rounded-sm" onClick={() => navigate('/signin')}>
+              Login
+            </button>
+            <button className="w-full text-coral bg-[#FBEFEE] text-md font-semibold p-2 hover:bg-[#fddddd] transition-colors rounded-sm" onClick={() => navigate('/signup')}>
+              Sign Up
+            </button>
+          </div>
+      }
+    </div>
+  )
+}
+
+
 
 const NavBar = () => {
   const navigate = useNavigate()
   const token = Cookies.get('token')
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
 
   return (
-    <div className="w-full h-16 flex justify-between items-center border-b-[0.5px] border-gray-300 px-28">
+    <div className="w-full h-16 flex justify-between items-center border-b-[0.5px] border-gray-300 px-6 sm:px-28">
       <div className="cursor-pointer"><img src="https://routine.co/_nuxt/logo.qmXltfCZ.svg" className="h-28 w-32" onClick={() => navigate('/')} /></div>
-      <div>
+
+      <div className="sm:hidden">
         {
-          token ? <button className="text-coral bg-[#FBEFEE] text-md font-semibold mr-2 px-3 py-2 rounded-md cursor-pointer hover:bg-[#fddddd] transition-colors" onClick={() => navigate('/dashboard')}>
-            Dashboard</button> :
-            <button className="text-coral bg-[#FBEFEE] text-md font-semibold mr-2 px-3 py-2 rounded-md cursor-pointer hover:bg-[#fddddd] transition-colors" onClick={() => navigate('/signin')}>
-              Login
-            </button>
+          isMenuOpen ? <XMarkIcon className="h-8 w-8 text-coral" onClick={() => setIsMenuOpen(false)} /> :
+            <Bars3Icon className="h-8 w-8 text-coral" onClick={() => setIsMenuOpen(true)} />
         }
+      </div>
+      {
+        isMenuOpen && <div className="absolute top-16 right-0 bg-white shadow-lg rounded-lg ">
+          <MenuBar />
+        </div>
+      }
+
+      <div className="hidden sm:block">
         {
-          token ? <button className="text-coral bg-[#FBEFEE] text-md font-semibold mr-8 px-3 py-2 rounded-md cursor-pointer hover:bg-[#fddddd] transition-colors" onClick={() => {
-            Cookies.remove('token')
-            navigate('/')
-          }}>Logout</button> :
-            <button className="text-coral bg-[#FBEFEE] text-md font-semibold mr-8 px-3 py-2 rounded-md cursor-pointer hover:bg-[#fddddd] transition-colors" onClick={() => navigate('/signup')}>
-              Sign Up
-            </button>
+          token ?
+            <div>
+              <button className="text-coral bg-[#FBEFEE] text-md font-semibold mr-2 px-3 py-2 rounded-md cursor-pointer hover:bg-[#fddddd] transition-colors" onClick={() => navigate('/dashboard')}>
+                Dashboard</button>
+              <button className="text-coral bg-[#FBEFEE] text-md font-semibold mr-8 px-3 py-2 rounded-md cursor-pointer hover:bg-[#fddddd] transition-colors" onClick={() => {
+                Cookies.remove('token')
+                navigate('/')
+              }}>Logout</button>
+            </div>
+            :
+            <div>
+              <button className="text-coral bg-[#FBEFEE] text-md font-semibold mr-2 px-3 py-2 rounded-md cursor-pointer hover:bg-[#fddddd] transition-colors" onClick={() => navigate('/signin')}>
+                Login
+              </button>
+              <button className="text-coral bg-[#FBEFEE] text-md font-semibold mr-8 px-3 py-2 rounded-md cursor-pointer hover:bg-[#fddddd] transition-colors" onClick={() => navigate('/signup')}>
+                Sign Up
+              </button>
+            </div>
         }
       </div>
     </div>
--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,10 +1,13 @@
-
+import { useNavigate } from "react-router-dom"
 const Home = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div className="w-full h-[600px] flex flex-col justify-center items-center">
-            <h1 className="text-6xl mb-4 font-bold text-zinc-800">The all-in-one work platform</h1>
-            <p className="text-2xl mb-16 text-zinc-700 text-center">Routine is the next-generation platform for professionals<br /> and teams to get things done faster!</p>
-            <button className="text-md bg-coral px-4 py-3 rounded-md text-white">Get Started</button>
+        <div className="w-full h-[calc(100vh-4rem)] flex flex-col justify-center items-center px-4">
+            <h1 className="text-center text-5xl md:text-6xl mb-4 font-bold text-zinc-800">The all-in-one work platform</h1>
+            <p className="text-center text-lg md:text-2xl mb-16 text-zinc-700 ">Routine is the next-generation platform for professionals<br /> and teams to get things done faster!</p>
+            <button className="text-md bg-coral px-4 py-3 rounded-md text-white" onClick={()=>navigate('/signup')}>Get Started</button>
         </div>
     )
 }

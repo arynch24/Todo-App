8c05f4f0234c0a472b4a9af01a9138fd6f82fe6d
Aryan <arynch24@gmail.com>
2025-05-06 11:42:24 +0530

    Add token verification to ProtectedRoute component

--- Frontend/src/routes/ProtectedRoute.tsx
+++ Frontend/src/routes/ProtectedRoute.tsx
@@ -1,27 +1,32 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const ProtectedRoute = ({ children }: any) => {
   const navigate = useNavigate();
+  const [isVerified, setIsVerified] = useState(false);
 
   useEffect(() => {
     const checkToken = async () => {
-      const res = await axios.get("https://routine-jf3l.onrender.com/api/user/verify",
-        {
+      try {
+        const res = await axios.get("https://routine-jf3l.onrender.com/api/user/verify", {
           withCredentials: true,
         });
 
-      if (res.status !== 200) {
+        if (res.status === 200) {
+          setIsVerified(true);
+        } else {
+          navigate("/signin");
+        }
+      } catch (err) {
         navigate("/signin");
       }
-    }
+    };
 
     checkToken();
-  }
-    , [navigate]);
+  }, [navigate]);
 
-  return children;
+  return isVerified ? children : null;
 };
 
 export default ProtectedRoute;

970718e32539f08fa96ccfeed94ebc3f4a46eab3
Aryan <arynch24@gmail.com>
2025-05-10 18:22:43 +0530

    Add FullCalendar and configure Calendar component

--- Frontend/src/components/Dashboard/Calendar.tsx
+++ Frontend/src/components/Dashboard/Calendar.tsx
@@ -1,10 +1,47 @@
+// Calendar.js
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
-const Calendar = () => {
+export default function CalendarComponent() {
   return (
-    <div>
-      I am the calendar page
+    <div className="h-full w-full overflow-clip">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        allDaySlot={false}
+        headerToolbar={{
+          left: 'title prev,next',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }}
+        slotLabelFormat={{
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        }}
+        eventTimeFormat={{
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        }}
+        editable={true}
+        selectable={true}
+        events={[
+          { title: "Meeting", date: "2025-05-11" },
+          // Add dynamic events later
+        ]}
+        dateClick={(info) => {
+          alert("Clicked date: " + info.dateStr);
+        }}
+        slotDuration={"01:00:00"}
+        slotLabelInterval={"01:00:00"}
+        dayHeaderFormat={{ weekday: 'short', day: '2-digit', omitCommas: true }}
+        titleFormat={{
+          year: 'numeric',
+          month: 'short',
+        }}
+      />
     </div>
-  )
+  );
 }
-
-export default Calendar

4199dd86cf9b7279d8561f4ec7809769dccaeb41
Aryan <arynch24@gmail.com>
2025-05-12 14:29:47 +0530

    Add token verification and redirect logic in Home component

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,13 +1,30 @@
 import { useNavigate } from "react-router-dom"
+import axios from "axios";
+
 const Home = () => {
 
     const navigate = useNavigate()
+
+    const checkToken = async () => {
+        try{
+            const res = await axios.get("https://routine-jf3l.onrender.com/api/user/verify", {
+                withCredentials: true,
+            });
+    
+            if (res.status === 200) {
+                navigate("/dashboard/agenda");
+            }
+        }
+        catch(err){
+            navigate("/signin");
+        }
+    };
 
     return (
         <div className="w-full h-[calc(100vh-4rem)] flex flex-col justify-center items-center px-4">
             <h1 className="text-center text-5xl md:text-6xl mb-4 font-bold text-zinc-800">The all-in-one work platform</h1>
             <p className="text-center text-lg md:text-2xl mb-16 text-zinc-700 ">Routine is the next-generation platform for professionals<br /> and teams to get things done faster!</p>
-            <button className="text-md bg-coral px-4 py-3 rounded-md text-white" onClick={()=>navigate('/signup')}>Get Started</button>
+            <button className="text-md bg-coral px-4 py-3 rounded-md text-white" onClick={checkToken}>Get Started</button>
         </div>
     )
 }

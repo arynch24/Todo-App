4a81c4621d9624b474df382e3b060ae2f897a896
Aryan <arynch24@gmail.com>
2025-05-14 13:31:30 +0530

    Refactor Settings component and remove unused code

--- Frontend/src/components/Dashboard/Settings.tsx
+++ Frontend/src/components/Dashboard/Settings.tsx
@@ -1,13 +1,11 @@
-import axios from "axios";
-import { useNavigate, NavLink, Outlet } from "react-router-dom";
-import { useState } from "react";
+import { NavLink, Outlet } from "react-router-dom";
 import { UserRoundPen, CircleEllipsis, Settings2 } from 'lucide-react';
 import { useLocation } from "react-router-dom";
 
 const SettingsSidebar = () => {
   const location = useLocation();
   return (
-    <div className="w-64 bg-white h-screen border-r-[0.5px] border-gray-300 flex flex-col  py-10">
+    <div className="w-64 bg-white h-[calc(100vh-4rem)] border-r-[0.5px] border-gray-300 flex flex-col  py-10">
       <h2 className="px-4 text-xs text-zinc-400">ACCOUNT</h2>
       <ul className="w-full py-3 px-3 flex flex-col gap-1">
         <NavLink to="/dashboard/settings">
@@ -37,20 +35,6 @@ const SettingsSidebar = () => {
 }
 
 const Settings = () => {
-  const navigate = useNavigate();
-  const [isVerified, setIsVerified] = useState(false);
-
-  const logout = async () => {
-    try {
-      await axios.get("https://routine-jf3l.onrender.com/api/user/signout", {
-        withCredentials: true,
-      });
-      navigate("/");
-      setIsVerified(false);
-    } catch (err) {
-      console.error(err);
-    }
-  };
   return (
     <div className="flex">
       <SettingsSidebar />
